a7060f8646c4bccd4f1daa45b296e1c691d7b1ed
Abrar051 <75354845+Abrar051@users.noreply.github.com>
2021-07-18 08:57:12 +0600

    Add files via upload

---
+++ java script basics/function.js
@@ -0,0 +1,17 @@
+var myGlobal=10;
+function fun1()
+{
+    global1 = 5;
+}
+function fun2()
+{
+    var output="";
+    if (typeof myglobal!="undefined")
+    {
+        output+="myGlobal"+myGlobal;
+    }
+    if (typeof global1!="undefined"){
+        
+
+    }
+}
